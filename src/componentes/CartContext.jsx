68bd618db1e9bcf56ba98ff72ff4c052ab8f146c
antoabrilx <antoaldalla@gmail.com>
2025-06-26 21:36:23 -0300

    Creando componente CartContext.jsx

---
+++ src/componentes/CartContext.jsx
@@ -0,0 +1,34 @@
+// =====================
+// CONTEXTO GLOBAL: CartContext.jsx
+// =====================
+import React, { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+export const useCart = () => useContext(CartContext);
+
+export const CartProvider = ({ children }) => {
+const [cart, setCart] = useState([]);
+
+const addToCart = (producto) => {
+    const existe = cart.find((item) => item.id === producto.id);
+    if (existe) {
+    setCart(cart.map((item) => item.id === producto.id ? { ...item, cantidad: item.cantidad + 1 } : item));
+    } else {
+    setCart([...cart, { ...producto, cantidad: 1 }]);
+    }
+};
+
+const removeFromCart = (id) => {
+    setCart(cart.filter((item) => item.id !== id));
+};
+
+const clearCart = () => setCart([]);
+
+  const total = cart.reduce((sum, item) => sum + item.precio * item.cantidad, 0);
+
+return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, total }}>
+    {children}
+    </CartContext.Provider>
+);
+};
