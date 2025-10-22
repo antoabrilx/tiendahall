a07bcd5362387372a8e7ac95accc50d2b3d4793f
Retamax <retamax071@gmail.com>
2025-10-22 18:51:41 -0300

    cambiando imagen de fondo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import "../styles/Hero.css";
 import { Link } from "react-router-dom";
-import fondoVinos from "../img/vino2.jpg";
+import fondoVinos from "../img/fondo.jpg";
 
 export default function Hero() {
   return (
